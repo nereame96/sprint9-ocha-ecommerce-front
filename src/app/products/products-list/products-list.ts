b406e73ca418b0a4acc15b0d01de4c6830bcc191
nereame96 <nereame96@gmail.com>
2026-03-13 10:23:20 +0100

    feat: implement category filter for product-list #3

--- src/app/products/products-list/products-list.ts
+++ src/app/products/products-list/products-list.ts
@@ -3,6 +3,7 @@ import { ProductsService } from '../../core/services/products.service';
 import { Router, RouterLink } from '@angular/router';
 import { ProductDetailComponent } from '../product-detail/product-detail';
 import { CartService } from '../../core/services/cart.service';
+import { Category } from '../../core/enums/category.enum';
 
 @Component({
   selector: 'app-products-list',
@@ -17,15 +18,22 @@ export class ProductsListComponent implements OnInit {
   cartService = inject(CartService)
   private router = inject(Router)
 
-  products = this.productsService.products
-  isLoading = this.productsService.loading
-  error = this.productsService.error
+  filteredProducts = this.productsService.filteredProducts;
+  selectedCategory = this.productsService.selectedCategory;
+  categoryList = this.productsService.categoryList;
+
+  isLoading = this.productsService.loading;
+  error = this.productsService.error;
 
 
 
   ngOnInit(): void {
     this.productsService.loadProducts()
 
+  }
+
+  onSelectCategory(category: Category | null) {
+    this.productsService.setCategoryFilter(category);
   }
 
 
